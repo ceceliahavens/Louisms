0680ea923c28d430376f71b6a36115587540ee0c
ceceliahavens <ceceliahavens@hotmail.com>
2015-10-02 00:49:22 +0000

    create quote

--- app/controllers/tributes_controller.rb
+++ app/controllers/tributes_controller.rb
@@ -6,4 +6,14 @@ class TributesController < ApplicationController
 	def new
 		@tributes = Tribute.new
 	end
+
+	def create
+		Tribute.create(tribute_params)
+		redirect_to root_path
+	end
+
+	private
+	def tribute_params
+		params.require(:tribute).permit(:saying, :contributor)
+	end
 end
