a29fa791537a06d8fe110ca0c1e4693850b930c6
ceceliahavens <ceceliahavens@hotmail.com>
2015-10-25 22:53:46 +0000

    validations

--- app/controllers/tributes_controller.rb
+++ app/controllers/tributes_controller.rb
@@ -8,7 +8,10 @@ class TributesController < ApplicationController
 	end
 
 	def create
-		Tribute.create(tribute_params)
+		@tribute = Tribute.create(tribute_params)
+		if @tribute.invalid?
+			flash[:error] = '<strong> Could not save</strong> the data you entered is invalid'
+		end
 		redirect_to root_path
 	end
 
--- app/models/tribute.rb
+++ app/models/tribute.rb
@@ -1,2 +1,5 @@
 class Tribute < ActiveRecord::Base
+	validates :saying, :presence => true, :length  => { :maximum => 1400, :minimum => 3 }
+	validates :contributor, :presence => true, :length => { :maximum => 60, :minimum => 3}
 end
+
